7adde5fe8a9c6f2ff2ef220338fd2e89a108997c
Amon <y96mt9rmsb@privaterelay.appleid.com>
2026-04-20 19:03:39 -0500

    feature test in git

--- obj/Debug/net9.0/AmonMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/AmonMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmonMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f3e3ee57cd12262d848c197d4644b7612ec754b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c19b3ca331b4db570f260ecb80721290100e6cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmonMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmonMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
