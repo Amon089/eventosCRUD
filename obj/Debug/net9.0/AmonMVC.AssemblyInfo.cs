c0ee08df721beb8b67efc9584a675f668cc3f6b7
Amon <y96mt9rmsb@privaterelay.appleid.com>
2026-04-20 19:55:38 -0500

    update

--- obj/Debug/net9.0/AmonMVC.AssemblyInfo.cs
+++ obj/Debug/net9.0/AmonMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmonMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b38b373e9915667dc3d3d454b0012653aa642c1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5722c7eb4da323355f220b3dd5fff310f0d241d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmonMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmonMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
